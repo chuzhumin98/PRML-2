a1e19d1f75a1237e03f4e3d42594d4259d104724
chuzhumin98 <chuzhumin98@gmail.com>
2018-03-18 21:57:04 +0800

    calculate the expectation and variance of square window

--- HW2/Program1.m
+++ HW2/Program1.m
@@ -1,9 +1,10 @@
+clear all
 pfun = @(x) 0.2*normpdf(x,-1,1)+0.8*normpdf(x,1,1);
 delta = 0.01;
 x = -5:delta:5;
 y = pfun(x);
 %% 生成随机数
-n = 100; %所生成的随机数数量
+n = 1; %所生成的随机数数量
 ita = 0.8; %两类的临界点
 X0 = randn(n, 1); %初始的随机数
 flag = rand(n, 1); %判断属于哪类
@@ -40,3 +41,27 @@ end
 % legend('p(x)的p.d.f.','p_n(x)的p.d.f.')
 % box on
 % saveas(gcf, strcat('n=',num2str(n),',a=',num2str(a),'时方窗拟合效果图.png'))
+%% 求解\epsilon(p_n)的均值方差在不同参数下
+m = 50; %取重采样次数为50
+n = 100; %p_n中n的大小
+a = 2; %参数a的取值
+X0 = [];
+flag = [];
+Xrandom = [];
+yn = [];
+for i=1:m
+    X0 = randn(n, 1); %初始的随机数
+    flag = rand(n, 1); %判断属于哪类
+    Xrandom = X0 + (flag < ita) - (flag >= ita); %生成的n个随机数
+    pnfun = @(x) 0; %定义一个Parzen窗估计函数
+    for i = 1:n 
+        pnfun = @(x) pnfun(x) + (x <= Xrandom(i)+a/2 && x >= Xrandom(i)-a/2)/(a*n);
+    end
+    for j = 1:length(x)
+        yn(j) = pnfun(x(j));
+    end
+    deltaP = (yn - y) .* (yn - y);
+    epsilon(i) = delta*sum(deltaP);
+end
+expect = mean(epsilon)
+variance = var(epsilon)

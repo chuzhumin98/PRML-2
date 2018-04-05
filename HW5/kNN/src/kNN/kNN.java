3292955397512d4a50a6eeb78d31ad1dd17c9ba4
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-05 21:27:10 +0800

    complete MNN for java version

--- HW5/kNN/src/kNN/kNN.java
+++ HW5/kNN/src/kNN/kNN.java
@@ -7,8 +7,8 @@ public class kNN {
 	public ReadData trainSet; //训练集数据 
 	public ReadData testSet; //测试集数据
 	
-	public int usedTrainSize = 5000; //使用的训练集大小
-	public int usedTestSize = 1000; //使用的测试集大小
+	public int usedTrainSize = 60000; //使用的训练集大小
+	public int usedTestSize = 10; //使用的测试集大小
 	
 	ArrayList<Integer> trainIndex = new ArrayList<Integer>(); //训练集的随机抽样数组
 	ArrayList<Integer> testIndex = new ArrayList<Integer>(); //测试集的随机抽样数组
@@ -46,21 +46,45 @@ public class kNN {
 	 * @return
 	 */
 	public double[] doMNN(int p) {
-		int maxIndex = -1; //最近邻的索引值
-		double minDist = Double.MAX_VALUE; //最近邻的距离
-		return null;
+		long time1 = System.currentTimeMillis(); //记录当前时间戳
+		int countRight = 0; //记录总的打对了的标签
+		for (int i = 0; i < this.usedTestSize; i++) {
+			int maxIndex = -1; //最近邻的索引值
+			double minDist = Double.MAX_VALUE; //最近邻的距离
+			for (int j = 0; j < this.usedTrainSize; j++) {
+				double myDist = 0.0;
+				if (p > 150) {
+					myDist = Distance.distanceChebychev(this.trainSet.images[this.trainIndex.get(j)], 
+							this.testSet.images[this.testIndex.get(i)]);
+				} else {
+					myDist = Distance.distanceNorm(this.trainSet.images[this.trainIndex.get(j)], 
+							this.testSet.images[this.testIndex.get(i)], p);
+				}
+				if (myDist < minDist) {
+					maxIndex = j;
+					minDist = myDist;
+				}
+			}
+			if (this.testSet.labels[this.testIndex.get(i)] 
+					== this.trainSet.labels[this.trainIndex.get(maxIndex)]) { //判断是否打对标签
+				countRight++;
+			}
+			System.out.println("no "+(i+1)+": real_"+this.testSet.labels[this.testIndex.get(i)]
+		              +" test_"+this.trainSet.labels[this.trainIndex.get(maxIndex)]);
+		}
+		double accuracy = 1.0*countRight/this.usedTestSize; //计算准确率
+		long time2 = System.currentTimeMillis();
+		double costTime = (double)(time2-time1);
+		System.out.println("accuracy:"+accuracy);
+		System.out.println("cost time:"+costTime+" ms");
+		double[] out = {accuracy, costTime};
+		return out;
 	}
 	
 	
 	
 	public static void main(String[] args) {
 		kNN knn = new kNN();
-		System.out.println(Double.MAX_VALUE);
-		System.out.println(Distance.distanceNorm(knn.trainSet.images[0], knn.testSet.images[0], 1));
-		System.out.println(Distance.distanceNorm(knn.trainSet.images[0], knn.testSet.images[0], 2));
-		System.out.println(Distance.distanceNorm(knn.trainSet.images[0], knn.testSet.images[0], 3));
-		System.out.println(Distance.distanceNorm(knn.trainSet.images[0], knn.testSet.images[0], 19));
-		System.out.println(Distance.distanceNorm(knn.trainSet.images[0], knn.testSet.images[0], 100));
-		System.out.println(Distance.distanceChebychev(knn.trainSet.images[0], knn.testSet.images[0]));
+		knn.doMNN(1);
 	}
 }

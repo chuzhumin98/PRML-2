212a02e4f4d215d2916a06675213069f16426920
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-06 01:19:26 +0800

    output the evaluation

--- HW5/python version/Evaluate.py
+++ HW5/python version/Evaluate.py
@@ -1,4 +1,5 @@
 from MNN import *
+import xlwt
 
 trainSizeSet = [50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 60000]
 evaluate = []
@@ -8,4 +9,11 @@ for i in range(1):
     evaluate.append(sample)
 
 print(evaluate)
+book = xlwt.Workbook(encoding='utf-8', style_compression=0)
+sheet = book.add_sheet('MNN', cell_overwrite_ok=True)
+for i in range(len(evaluate)):
+    sheet.write(i, 0, trainSizeSet[i])
+    sheet.write(i, 1, evaluate[i][0])
+    sheet.write(i, 2, evaluate[i][0])
 
+book.save("output/MNN_trainSize.xlsx")

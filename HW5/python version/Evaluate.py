84d116c23a8dd698aa97ae702d8f43d5edbfc24c
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-06 02:02:39 +0800

    calculate KNN along different K value

--- HW5/python version/Evaluate.py
+++ HW5/python version/Evaluate.py
@@ -1,18 +1,42 @@
 from MNN import *
 import xlwt
 
-trainSizeSet = [50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 60000]
-evaluate = []
-for i in range(len(trainSizeSet)):
-    sample = doMNN(trainSizeSet[i])
-    evaluate.append(sample)
+# evaluate MNN along different train size
+def evaluaeTrainSize():
+    trainSizeSet = [50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 60000]
+    evaluate = []
+    for i in range(len(trainSizeSet)):
+        sample = doMNN(trainSizeSet[i])
+        evaluate.append(sample)
 
-print(evaluate)
-book = xlwt.Workbook(encoding='utf-8', style_compression=0)
-sheet = book.add_sheet('MNN', cell_overwrite_ok=True)
-for i in range(len(evaluate)):
-    sheet.write(i, 0, trainSizeSet[i])
-    sheet.write(i, 1, evaluate[i][0])
-    sheet.write(i, 2, evaluate[i][1])
+    print(evaluate)
+    book = xlwt.Workbook(encoding='utf-8', style_compression=0)
+    sheet = book.add_sheet('MNN', cell_overwrite_ok=True)
+    for i in range(len(evaluate)):
+        sheet.write(i, 0, trainSizeSet[i])
+        sheet.write(i, 1, evaluate[i][0])
+        sheet.write(i, 2, evaluate[i][1])
 
-book.save("output/MNN_trainSize.xlsx")
+    book.save("output/MNN_trainSize.xlsx")
+
+
+# evaluate KNN with different k value
+def evaluateKValue():
+    kSet = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
+    evaluate = []
+    for i in range(len(kSet)):
+        if (i != 0):
+            sample = doKNN(kSet[i])
+        else:
+            sample = doMNN() #when k=1, use method doMNN()
+        evaluate.append(sample)
+
+    print(evaluate)
+    book = xlwt.Workbook(encoding='utf-8', style_compression=0)
+    sheet = book.add_sheet('KValue', cell_overwrite_ok=True)
+    for i in range(len(evaluate)):
+        sheet.write(i, 0, kSet[i])
+        sheet.write(i, 1, evaluate[i][0])
+        sheet.write(i, 2, evaluate[i][1])
+
+    book.save("output/KNN_KValue.xlsx")

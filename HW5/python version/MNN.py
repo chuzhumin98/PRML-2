e166a7c52438f4a9da023c523e5986ebe6f28d36
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-05 23:43:34 +0800

    calculate for distance

--- HW5/python version/MNN.py
+++ HW5/python version/MNN.py
@@ -1,85 +1,15 @@
-import numpy as np
-import struct
+from ReadData import *
 
-# 给出各文件的路径
-trainimagepath = "input/train_images"
-trainlabelpath = "input/train_labels"
-testimagepath = "input/test_images"
-testlabelpath = "input/test_labels"
+# 对每张图片进行所有训练集的p-范数距离求解
+def distance(train, image, p):
+    test = np.tile(image, (trainSize, 1, 1))
+    delta = train - test
+    if (p <= 150):
+        delta = delta**p
+        sums = np.sum(np.abs(delta),axis=(1,2))
+        return sums**(1.0/p)
+    else:
+        return np.max(np.abs(delta), axis=(1,2))
 
-# 设置一些常量的值
-trainSize = 60000
-testSize = 10000
-row = 28
-column = 28
-
-#申明一些数据结构
-trainImage = []
-trainLabel = []
-testImage = []
-testLabel = []
-
-# 读入训练样本
-f = open(trainimagepath, 'rb')
-string = f.read()
-print('train image file size:',len(string))
-index=16
-for i in range(trainSize):
-    oneimage = []
-    for j in range(row):
-        onerow = np.zeros(row)
-        [onerow[0], onerow[1], onerow[2], onerow[3], onerow[4], onerow[5], onerow[6],
-         onerow[7], onerow[8], onerow[9], onerow[10], onerow[11], onerow[12], onerow[13],
-         onerow[14], onerow[15], onerow[16], onerow[17], onerow[18], onerow[19], onerow[20],
-         onerow[21], onerow[22], onerow[23], onerow[24], onerow[25], onerow[26], onerow[27]] = struct.unpack('28B',string[index:index+28])
-        index += 28
-        oneimage.append(onerow)
-    imagematrix = np.mat(oneimage)
-    trainImage.append(imagematrix)
-    if ((i+1)%10000 == 0):
-        print("has read ",(i+1)," images in train file")
-
-# 读入训练标签
-f = open(trainlabelpath, 'rb')
-string = f.read()
-print('train label file size:',len(string))
-index = 8
-for i in range(trainSize):
-    [label] = struct.unpack('B',string[index:index+1])
-    index += 1
-    trainLabel.append(label)
-
-print(trainLabel)
-
-# 读入测试样本
-f = open(testimagepath, 'rb')
-string = f.read()
-print('test image file size:',len(string))
-index=16
-for i in range(testSize):
-    oneimage = []
-    for j in range(row):
-        onerow = np.zeros(row)
-        [onerow[0], onerow[1], onerow[2], onerow[3], onerow[4], onerow[5], onerow[6],
-         onerow[7], onerow[8], onerow[9], onerow[10], onerow[11], onerow[12], onerow[13],
-         onerow[14], onerow[15], onerow[16], onerow[17], onerow[18], onerow[19], onerow[20],
-         onerow[21], onerow[22], onerow[23], onerow[24], onerow[25], onerow[26], onerow[27]] = struct.unpack('28B',string[index:index+28])
-        index += 28
-        oneimage.append(onerow)
-    imagematrix = np.mat(oneimage)
-    testImage.append(imagematrix)
-    if ((i+1)%10000 == 0):
-        print("has read ",(i+1)," images in test file")
-
-
-# 读入测试标签
-f = open(testlabelpath, 'rb')
-string = f.read()
-print('test label file size:',len(string))
-index = 8
-for i in range(testSize):
-    [label] = struct.unpack('B',string[index:index+1])
-    index += 1
-    testLabel.append(label)
-
-print(testLabel)
+[trainImage, trainLabel, testImage, testLabel] = importData()
+distance(trainImage, testImage[0], 2)

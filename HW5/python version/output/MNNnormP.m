fd76f97c4f6cec2a202761d78eb6034e567209fe
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-07 01:31:13 +0800

    plot for MNN norm p

--- HW5/python version/output/MNNnormP.m
+++ HW5/python version/output/MNNnormP.m
@@ -1,4 +1,4 @@
-[result] = xlsread('KNN_KValue.xls');
+[result] = xlsread('MNN_NormP.xls');
 size = length(result);
 [AX,H1,H2] = plotyy(1:size, result(:,2),1:size, result(:,3),'plot');
 set(AX(1),'XColor','k','YColor','k');
@@ -13,16 +13,16 @@ set(H1,'LineStyle','-','LineWidth',2,'MarkerSize',20);
 set(H1,'color','b');
 set(H2,'LineStyle','-.');
 set(H2,'color','r','LineWidth',2,'MarkerSize',20);
-set(AX(1),'yTick',[75:5:90])  %设置左边Y轴的刻度 
-set(AX(2),'yTick',[0.935:0.005:0.955]) %设置右边Y轴的刻度
-set(AX(1),'ylim',[75 90])  %设置左边Y轴的刻度 
-set(AX(2),'ylim',[0.935 0.955]) %设置右边Y轴的刻度
+set(AX(1),'yTick',[60:30:180])  %设置左边Y轴的刻度 
+set(AX(2),'yTick',[0.7:0.1:1.0]) %设置右边Y轴的刻度
+set(AX(1),'ylim',[60 180])  %设置左边Y轴的刻度 
+set(AX(2),'ylim',[0.7 1.0]) %设置右边Y轴的刻度
 xlim([0.5 size+0.5])
 set(gca, 'xTick', 1:size);  
-set(gca,'XTickLabel',{'1','3','5','7','9','11','13','15','17','19','21'}) 
-legend('分类所花时间','模型准确率','location','northwest');
-xlabel('k的取值');
-title('k-近邻性能随着k的取值的变化关系');
+set(gca,'XTickLabel',{'1','2','3','4','5','10','20','200'}) 
+legend('分类所花时间','模型准确率','location','southwest');
+xlabel('范数p的取值');
+title('最近邻性能随着距离度量的变化关系');
 grid on
 box on
-saveas(gcf,'KNNKValue.png')
+saveas(gcf,'MNNNormP.png')

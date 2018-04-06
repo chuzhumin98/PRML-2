e9e28b2c380b03f64059b0ec8bf5950a3b578502
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-06 23:41:49 +0800

    plot for MNN train size

---
+++ HW5/python version/output/MNNtrainSize.m
@@ -0,0 +1,28 @@
+[result] = xlsread('MNN_trainSize.xls');
+size = length(result);
+[AX,H1,H2] = plotyy(1:size, result(:,2),1:size, result(:,3),'plot');
+set(AX(1),'XColor','k','YColor','k');
+set(AX(2),'XColor','k','YColor','k');
+HH1=get(AX(1),'Ylabel');
+set(HH1,'String','分类所花时间(s)');
+set(HH1,'color','k');
+HH2=get(AX(2),'Ylabel');
+set(HH2,'String','分类准确率');
+set(HH2,'color','k');
+set(H1,'LineStyle','-','LineWidth',2,'MarkerSize',20);
+set(H1,'color','b');
+set(H2,'LineStyle','-.');
+set(H2,'color','r','LineWidth',2,'MarkerSize',20);
+set(AX(1),'yTick',[0:100:500])  %设置左边Y轴的刻度 
+set(AX(2),'yTick',[0.6:0.1:1.0]) %设置右边Y轴的刻度
+set(AX(1),'ylim',[0 520])  %设置左边Y轴的刻度 
+set(AX(2),'ylim',[0.6 1.0]) %设置右边Y轴的刻度
+xlim([0.5 size+0.5])
+set(gca, 'xTick', 1:size);  
+set(gca,'XTickLabel',{'50','100','200','500','1000','2000','5000','10000','20000','60000'}) 
+legend('分类所花时间','模型准确率','location','northwest');
+xlabel('训练集规模');
+title('最近邻性能随着训练集规模的变化关系');
+grid on
+box on
+saveas(gcf,'MNNtrainsize.png')
